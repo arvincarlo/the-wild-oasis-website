6eb980c499c876ba7a651b2f50e87d01d041278d
Arvin Cris <arvincris97@gmail.com>
2025-01-27 20:14:57 +0800

    Creating the Layout

--- app/page.js
+++ app/page.js
@@ -1,10 +1,8 @@
 import Link from "next/link";
-import Navigation from "./components/Navigation";
 
 export default function Page() {
   return (
     <>
-      <Navigation/>
       <h1>The Wild Oasis. Welcome to paradise.</h1>
       {/* <a href="/cabins">Explore Luxury cabins</a> */}
       <Link href="/cabins">Explore Luxury cabins</Link>
