86a00990f45d1b0ee3eb20166bb20a06b1a95600
Arvin Cris <arvincris97@gmail.com>
2025-01-28 22:36:21 +0800

    Improving the Navigation and Root Layout

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,16 @@
 import Navigation from "@/app/_components/Navigation";
 import Logo from "@/app/_components/Logo";
 
-import {Josefin_Sans, Karla, Dosis, Exo_2} from "next/font/google";
+import {Josefin_Sans, Karla, Dosis, Exo_2, Indie_Flower} from "next/font/google";
 
 // run the function
 const font = Exo_2({
   subsets: ['latin'],
-  display: "swap", // this will display the text in default fonts, and once ready it will swap
+  display: "swap",// this will display the text in default fonts, and once ready it will swap
 });
 
 import '@/app/_styles/globals.css'
+import Header from "./_components/Header";
 
 export const metadata = {
   title: {
@@ -23,15 +24,13 @@ export const metadata = {
 export default function RootLayout({children}) {
   return(
     <html>
-      <body className={`${font.className} bg-primary-950 text-primary-100 min-h-screen`}>
-        <header>
-          <Logo></Logo>
-        </header>
-        <Navigation/>
-        <main>
-          {children}
-        </main>
-        <footer>Copyright by The Wild Oasis</footer>
+      <body className={`${font.className} bg-primary-950 text-primary-100 min-h-screen flex flex-col antialised`}>
+        <Header/>
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl mx-auto">
+            {children}
+          </main>
+        </div>
       </body>
     </html>
   )
