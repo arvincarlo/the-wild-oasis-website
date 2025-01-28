18da7a0f6d61f6cb6b22933a7d2299d15c602f94
Arvin Cris <arvincris97@gmail.com>
2025-01-28 22:23:14 +0800

    Loading and optimizing fonts

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,14 @@
 import Navigation from "@/app/_components/Navigation";
 import Logo from "@/app/_components/Logo";
 
+import {Josefin_Sans, Karla, Dosis, Exo_2} from "next/font/google";
+
+// run the function
+const font = Exo_2({
+  subsets: ['latin'],
+  display: "swap", // this will display the text in default fonts, and once ready it will swap
+});
+
 import '@/app/_styles/globals.css'
 
 export const metadata = {
@@ -9,13 +17,13 @@ export const metadata = {
     default: "Welcome - The Wild Oasis"
   },
   description: "Luxurious cabin hotel, located in the heart of the Italian Dolomites, surrounded by beatiful mountains and dark forests",
-  
+
 }
 
 export default function RootLayout({children}) {
   return(
     <html>
-      <body className="bg-primary-950 text-primary-100 min-h-screen">
+      <body className={`${font.className} bg-primary-950 text-primary-100 min-h-screen`}>
         <header>
           <Logo></Logo>
         </header>
