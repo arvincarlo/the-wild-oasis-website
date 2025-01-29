01a90de8bed841a28eb689c935405d1d8efc7d04
Arvin Cris <arvincris97@gmail.com>
2025-01-29 16:37:01 +0800

    Building the homepage

--- app/layout.js
+++ app/layout.js
@@ -24,7 +24,7 @@ export const metadata = {
 export default function RootLayout({children}) {
   return(
     <html>
-      <body className={`${font.className} bg-primary-950 text-primary-100 min-h-screen flex flex-col antialised`}>
+      <body className={`${font.className} bg-primary-950 text-primary-100 min-h-screen flex flex-col antialised relative`}>
         <Header/>
         <div className="flex-1 px-8 py-12">
           <main className="max-w-7xl mx-auto">
