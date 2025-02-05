9e619b271abcfdcc36b2cea5e1f529735a9248a6
Arvin Cris <arvincris97@gmail.com>
2025-02-05 09:42:45 +0800

    Deleting a Reservation

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -3,6 +3,7 @@
 import { revalidatePath } from "next/cache";
 import { auth, signIn, signOut } from "./auth";
 import { supabase } from "./supabase";
+import { getBookings } from "./data-service";
 
 export async function signInAction() {
     await signIn("google", {redirectTo: '/account'});
@@ -36,6 +37,30 @@ export async function updateGuest(formData) {
     }
 
     revalidatePath('/account/profile');
+}
+
+// Server Action
+export async function deleteReservation(bookingId) {
+    const session = await auth();
+
+    if (!session) throw new Error("You must be logged in");
+
+    // Protecting the action from unauthorized users
+    const guestBookings = await getBookings(session.user.guestId);
+    const guestBookingIds = guestBookings.map((booking) => booking.id);
+
+    if (!guestBookingIds.includes(bookingId)) {
+        throw new Error("You are not allowed to delete this booking");
+    }
+
+    const { data, error } = await supabase.from('bookings').delete().eq('id', bookingId);
+
+    if (error) {
+      throw new Error('Booking could not be deleted');
+    }
+
+    // revalidate the path once delete success
+    revalidatePath("/account/reservations");
 }
 
 function validateNationalID(nationalID) {
