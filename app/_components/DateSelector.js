6b58d0dce025720991a43292d0738b62c5508fc7
Arvin Cris <arvincris97@gmail.com>
2025-02-07 10:34:24 +0800

    Back to Cabins Page: Finishing the Date Selector

--- app/_components/DateSelector.js
+++ app/_components/DateSelector.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { isWithinInterval } from "date-fns";
+import { differenceInDays, isPast, isSameDay, isWithinInterval } from "date-fns";
 import { DayPicker } from "react-day-picker";
 import "react-day-picker/dist/style.css";
 import { useReservation } from "../_context/ReservationContext";
@@ -18,16 +18,18 @@ function isAlreadyBooked(range, datesArr) {
 function DateSelector({settings, cabin, bookedDates}) {
   const {range, setRange, resetRange} = useReservation();
 
-  // CHANGE
-  const regularPrice = 23;
-  const discount = 23;
-  const numNights = 23;
-  const cabinPrice = 23;
+  const displayRange = isAlreadyBooked(range, bookedDates) ? {} : range;
+
+  const {
+    regularPrice,
+    discount,
+  } = cabin;
+
+  const cabinPrice = regularPrice - discount;
+  const numNights = differenceInDays(displayRange.to, displayRange.from);
 
   // SETTINGS
   const {minBookingLength, maxBookingLength} = settings;
-  // const minBookingLength = 1;
-  // const maxBookingLength = 23;
 
   return (
     <div className="flex flex-col justify-between">
@@ -35,7 +37,7 @@ function DateSelector({settings, cabin, bookedDates}) {
         className="pt-12 place-self-center"
         mode="range"
         onSelect={setRange}
-        selected={range}
+        selected={displayRange}
         min={minBookingLength + 1}
         max={maxBookingLength}
         fromMonth={new Date()}
@@ -43,6 +45,7 @@ function DateSelector({settings, cabin, bookedDates}) {
         toYear={new Date().getFullYear() + 5}
         captionLayout="dropdown"
         numberOfMonths={2}
+        disabled={(currentDate) => isPast(currentDate) || bookedDates.some(date => isSameDay(date, currentDate))}
       />
 
       <div className="flex items-center justify-between px-8 bg-accent-500 text-primary-800 h-[72px]">
